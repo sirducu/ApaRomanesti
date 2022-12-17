3f0cc16e0ee143c324211f51cb98cd8c03326ba6
Radu Florian <florianmradu@gmail.com>
2022-12-17 11:27:42 +0200

    First Commit

---
+++ mtplib.py
@@ -0,0 +1,19 @@
+from email.message import EmailMessage
+import ssl, smtplib
+def smail(msg):
+    sender = 'pythonradu1@gmail.com'
+    password = 'eqnaknawmuvmqqjr'
+    receiver = 'florianmradu@gmail.com'
+    subject = 'WaterApp Anouncements'
+    body = f'''
+    {msg}
+    '''
+    em = EmailMessage()
+    em['From'] = sender
+    em['To'] = receiver
+    em['Subject'] = subject
+    em.set_content(body)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+       smtp.login(sender, password)
+       smtp.sendmail(sender, receiver, em.as_string())
